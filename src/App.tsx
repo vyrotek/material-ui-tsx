f96fe76137a9cc3c2450701c6a599dbb2f511d22
Jason Barnes <jason@sparkview.io>
2017-08-21 19:46:08 -0700

    Error Messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 
-// F12 (Go to Definition) not working - No definition found for 'Button'
-// Running 'npm start' works fine.
-// While running if you switch to the import below and it continues to works.
-// import Button from 'material-ui/Button';
+import Button from 'material-ui/Button';
+// VS Code cannot find definition for the above import and shows error:
+// Could not find a declaration file for module 'material-ui/Button'.
+// '/material-ui-tsx/node_modules/material-ui/Button/index.js'
+// implicitly has an 'any' type. Try `npm install @types/material-ui/Button` if it exists 
+// or add a new declaration (.d.ts) file containing `declare module 'material-ui/Button';`
 
-// F12 (Go to Definition) working - But running 'npm start' with this throws an error:
-// node_modules\material-ui\index.d.ts
-// (2122,25): error TS2307: Cannot find module 'enzyme'.
+ 
 // import { Button } from 'material-ui';
+// VS Code finds definition with the above import but react throws this error after 'npm start':
+// Failed to compile.
+// \material-ui-tsx\node_modules\material-ui\index.d.ts
+// (2122,25): error TS2307: Cannot find module 'enzyme'.
 
 export default class App extends React.Component {
 
